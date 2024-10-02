a89a68c818c9cfc505ee79bf3704b201077b9d6e
masashikuwahara <brucemclarenp4@gmail.com>
2024-10-02 23:38:40 +0900

    タスク完了、削除機能追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -13,7 +13,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        $tasks = Task::all();
+        $tasks = Task::where('status', false)->get();
         return view('tasks.index', compact('tasks'));
     }
 
@@ -67,26 +67,38 @@ class TaskController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        $rules = [
+        //「編集する」ボタンをおしたとき
+        if ($request->status === null) {
+            $rules = [
             'task_name' => 'required|max:100',
-          ];
+            ];
         
-          $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
+            $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
         
-          Validator::make($request->all(), $rules, $messages)->validate();
+            Validator::make($request->all(), $rules, $messages)->validate();
         
+            //該当のタスクを検索
+            $task = Task::find($id);
         
-          //該当のタスクを検索
-          $task = Task::find($id);
+            //モデル->カラム名 = 値 で、データを割り当てる
+            $task->name = $request->input('task_name');
         
-          //モデル->カラム名 = 値 で、データを割り当てる
-          $task->name = $request->input('task_name');
+            //データベースに保存
+            $task->save();
+        } else {
+            //「完了」ボタンを押したとき
         
-          //データベースに保存
-          $task->save();
+            //該当のタスクを検索
+            $task = Task::find($id);
         
-          //リダイレクト
-          return redirect('/tasks');
+            //モデル->カラム名 = 値 で、データを割り当てる
+            $task->status = true; //true:完了、false:未完了
+        
+            //データベースに保存
+            $task->save();
+        }
+        //リダイレクト
+        return redirect('/tasks');
     }
 
     /**
@@ -94,6 +106,7 @@ class TaskController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        Task::find($id)->delete();
+        return redirect('/tasks');
     }
 }
